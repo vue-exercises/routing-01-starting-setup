cc52e000c7221aff9b625909e0a72a5028009b61
dedoblege <dedoblege@gmail.com>
2020-10-09 21:23:10 +0200

    root metadata

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ const router = createRouter({
     { path: '/', redirect: '/teams' },
     {
       path: '/teams',
+      meta: { needsAuth: true },
       components: { default: TeamsList, footer: TeamsFooter },
       children: [
         {
@@ -51,7 +52,12 @@ const router = createRouter({
 router.beforeEach(function(to, from, next) {
   console.log('Global beforeEach');
   console.log(to, from);
-  next();
+  if (to.meta.needsAuth) {
+    console.log('Needs auth!');
+    next();
+  } else {
+    next();
+  }
 });
 
 router.afterEach(function(to, from) {
