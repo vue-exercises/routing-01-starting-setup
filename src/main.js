12e8c457c578fd8cc02afe19c667e18be9b31eab
dedoblege <dedoblege@gmail.com>
2020-10-09 21:30:17 +0200

    organizing main and router files

--- src/main.js
+++ src/main.js
@@ -1,69 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-
 import App from './App.vue';
-import TeamsList from './pages/TeamsList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import TeamsFooter from './pages/TeamsFooter.vue';
-import UsersList from './pages/UsersList.vue';
-import UsersFooter from './pages/UsersFooter.vue';
-import NotFound from './pages/NotFound.vue';
+import router from './router.js';
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      path: '/teams',
-      meta: { needsAuth: true },
-      components: { default: TeamsList, footer: TeamsFooter },
-      children: [
-        {
-          name: 'team-members',
-          path: ':teamId',
-          component: TeamMembers,
-          props: true
-        }
-      ]
-    },
-    {
-      path: '/users',
-      components: { default: UsersList, footer: UsersFooter },
-      beforeEnter(to, from, next) {
-        console.log('users beforeEnter');
-        console.log(to, from);
-        next();
-      }
-    },
-    { path: '/:notFound(.*)', component: NotFound }
-  ],
-  linkActiveClass: 'active',
-  scrollBehavior(_, _2, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return {
-      left: 0,
-      top: 0
-    };
-  }
-});
-
-router.beforeEach(function(to, from, next) {
-  console.log('Global beforeEach');
-  console.log(to, from);
-  if (to.meta.needsAuth) {
-    console.log('Needs auth!');
-    next();
-  } else {
-    next();
-  }
-});
-
-router.afterEach(function(to, from) {
-  console.log('Global aftereach');
-  console.log(to, from);
-});
 const app = createApp(App);
 
 app.use(router);
