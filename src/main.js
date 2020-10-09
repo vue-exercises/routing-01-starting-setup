b16dfc046675808423dfb74394a2c273dddef915
dedoblege <dedoblege@gmail.com>
2020-10-09 20:27:40 +0200

    redirecting & catch all

--- src/main.js
+++ src/main.js
@@ -5,13 +5,16 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import UsersList from './components/users/UsersList.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList, alias: '/' },
+    { path: '/', redirect: '/teams' },
+    { path: '/teams', component: TeamsList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
-    { path: '/users', component: UsersList }
+    { path: '/users', component: UsersList },
+    { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
 });
