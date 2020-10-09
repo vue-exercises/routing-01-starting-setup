9539b45e9e7b9b6593b5d976d84e1db5f3dec8e1
dedoblege <dedoblege@gmail.com>
2020-10-09 20:21:34 +0200

    redirecting urls using alias

--- src/main.js
+++ src/main.js
@@ -9,8 +9,7 @@ import UsersList from './components/users/UsersList.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList },
+    { path: '/teams', component: TeamsList, alias: '/' },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
     { path: '/users', component: UsersList }
   ],
